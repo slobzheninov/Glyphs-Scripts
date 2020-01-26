4cd35292422ae7246fde6095d2fed48ee7a459fa
slobzheninov <60325634+slobzheninov@users.noreply.github.com>
2020-01-26 20:07:34 +0100

    Add files via upload
    Align nodes with keyboard shortcuts

---
+++ Align Top.py
@@ -0,0 +1,37 @@
+#MenuTitle: Align Top
+# -*- coding: utf-8 -*-
+__doc__="""
+Align Top.
+"""
+
+
+
+Font = Glyphs.font
+Doc = Glyphs.currentDocument
+selectedLayer = Font.selectedLayers[0]
+
+try:
+	try:
+		# until v2.1:
+		selection = selectedLayer.selection()
+	except:
+		# since v2.2:
+		selection = selectedLayer.selection
+	
+	selectionYList = [ n.y for n in selection ]
+	lowestY, highestY = min( selectionYList ), max( selectionYList )
+	
+	Font.disableUpdateInterface()
+
+	sortedSelection = sorted( selection, key=lambda n: n.y)
+	for thisNodeIndex in range( len(selection)):
+		sortedSelection[thisNodeIndex].y = highestY
+			
+	Font.enableUpdateInterface()
+	
+except Exception, e:
+	if selection == ():
+		print "Cannot align nodes: nothing selected in frontmost layer."
+	else:
+		print "Error. Cannot align nodes:", selection
+		print e
