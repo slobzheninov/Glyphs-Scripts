80516b02f81078c61d3a4a96796955c62aa69118
slobzheninov <60325634+slobzheninov@users.noreply.github.com>
2020-01-31 23:12:19 +0100

    Add files via upload

---
+++ Align/Align Horizontal Center.py
@@ -0,0 +1,37 @@
+#MenuTitle: Align Horizontal Center
+# -*- coding: utf-8 -*-
+__doc__="""
+Align Horizontal Center.
+"""
+
+
+
+Font = Glyphs.font
+Doc = Glyphs.currentDocument
+selectedLayer = Font.selectedLayers[0]
+
+try:
+	try:
+		# until v2.1:
+		selection = selectedLayer.selection()
+	except:
+		# since v2.2:
+		selection = selectedLayer.selection
+	
+	selectionXList = [ n.x for n in selection ]
+	leftMostX, rightMostX = min( selectionXList ), max( selectionXList )
+	
+	Font.disableUpdateInterface()
+
+	sortedSelection = sorted( selection, key=lambda n: n.x)
+	for thisNodeIndex in range( len(selection)):
+		sortedSelection[thisNodeIndex].x = (leftMostX+rightMostX)/2
+			
+	Font.enableUpdateInterface()
+	
+except Exception, e:
+	if selection == ():
+		print "Cannot align nodes: nothing selected in frontmost layer."
+	else:
+		print "Error. Cannot align nodes:", selection
+		print e
