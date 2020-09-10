103efe595e685991225799b4976211628eb875a6
slobzheninov <60325634+slobzheninov@users.noreply.github.com>
2020-09-10 14:16:32 +0200

    Add files via upload

---
+++ Font Info/Preferred Names - Clean.py
@@ -0,0 +1,18 @@
+#MenuTitle: Clean Preferred Names
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function, unicode_literals
+__doc__="""
+Deletes preferredFamilyName and preferredSubfamilyName custom parameters.
+"""
+
+thisFont = Glyphs.font
+cpName = ["preferredFamilyName", "preferredSubfamilyName"]
+
+for thisInstance in thisFont.instances:
+	if thisInstance.active:
+		#print("Processing Instance:", thisInstance.name)
+		for j in range(len(cpName)):
+			for i in reversed(range(len(thisInstance.customParameters))):
+				if thisInstance.customParameters[i].name == cpName[j]:
+					del thisInstance.customParameters[i]
+					print("Deleted from:", thisInstance.name)
