a3b44412c330d00a9232c058291fd801abde0495
slobzheninov <60325634+slobzheninov@users.noreply.github.com>
2020-09-14 16:03:44 +0200

    Add files via upload

--- Font Info/Demo Instance Generator.py
+++ Font Info/Demo Instance Generator.py
@@ -13,7 +13,6 @@ from Foundation import NSUserDefaults, NSString
 m = 15 # margin
 tm = 35 # top/vertical margin
 bm = 50 # bottom margin
-h = 100 # text editor height
 
 class DemoFontsGenerator ( object ):
 	
@@ -35,13 +34,10 @@ class DemoFontsGenerator ( object ):
 		# UI elements:
 			
 		self.w.text_name = vanilla.TextBox( (m, m-3, windowWidth, tm), "Name (Suffix):", sizeStyle='small' )
-		self.w.text_features = vanilla.TextBox( (m, h*2+tm*2+m-3, windowWidth, tm), "Limited Character Set:", sizeStyle='small' )
-		self.w.text_chars = vanilla.TextBox( (m, h+tm+m-3, windowWidth, tm), "Limited Features:", sizeStyle='small' )
-
-		self.w.name = vanilla.TextEditor( (m, tm, windowWidth-m*2, tm), callback=self.SavePreferences, checksSpelling=False )
-		self.w.chars = vanilla.TextEditor( (m, tm*3, windowWidth-m*2, h), callback=self.SavePreferences, checksSpelling=False )
-		self.w.features = vanilla.TextEditor( (m, tm*4+h, windowWidth-m*2, h), callback=self.SavePreferences, checksSpelling=False )
+		self.w.text_glyphs = vanilla.TextBox( (m, tm*2+m-3, windowWidth, tm), "Limited Character Set:", sizeStyle='small' )
 		
+		self.w.name = vanilla.TextEditor( (m, tm, windowWidth-m*2, tm), callback=self.SavePreferences, checksSpelling=False )
+		self.w.glyphs = vanilla.TextEditor( (m, tm*3, windowWidth-m*2, -bm), callback=self.SavePreferences, checksSpelling=False )
 		
 		self.windowResize(None)
 		
@@ -51,7 +47,6 @@ class DemoFontsGenerator ( object ):
 		
 		# Reset Button:
 		self.w.resetButton = vanilla.Button(((-80-m)*2, -20-m, (-80-m)-m, m), "Reset", sizeStyle='regular', callback=self.ResetParameters )
-		#self.w.setDefaultButton( self.w.resetButton )
 		
 		self.w.bind("resize",self.windowResize)
 		
@@ -66,40 +61,32 @@ class DemoFontsGenerator ( object ):
 	
 	def windowResize( self, sender ):
 		windowWidth = self.w.getPosSize()[2]
-		windowHeight = self.w.getPosSize()[3]
 		adaptiveWidth = windowWidth - m*2
-		adaptiveHeight = windowHeight / 2 - (tm * 4 + bm) / 2
-		
-		self.w.text_features.setPosSize( (m, tm*2+m-3, adaptiveWidth, adaptiveHeight) )
-		self.w.text_chars.setPosSize( (m, adaptiveHeight+tm*3+m-3, adaptiveWidth, tm) )
-		
+				
 		self.w.name.setPosSize( (m, tm, adaptiveWidth, tm) )
-		self.w.chars.setPosSize( (m, tm*3, adaptiveWidth, adaptiveHeight) )
-		self.w.features.setPosSize( (m, tm*4+adaptiveHeight, adaptiveWidth, adaptiveHeight ) )
+		self.w.glyphs.setPosSize( (m, tm*3, adaptiveWidth, -bm) )
 		
 		
 	def SavePreferences( self, sender ):
 		try:
 			Glyphs.defaults["save.DemoInstancesGenerator.name"] = self.w.name.get()
-			Glyphs.defaults["save.DemoInstancesGenerator.chars"] = self.w.chars.get()
-			Glyphs.defaults["save.DemoInstancesGenerator.features"] = self.w.features.get()
+			Glyphs.defaults["save.DemoInstancesGenerator.glyphs"] = self.w.glyphs.get()
 		except:
 			return False
 			
 		return True
+
 
 	def LoadPreferences( self ):
 		try:
 			NSUserDefaults.standardUserDefaults().registerDefaults_(
 				{
 					"save.DemoInstancesGenerator.name": "Demo",
-					"save.DemoInstancesGenerator.chars": "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z a b c d e f g h i j k l m n o p q r s t u v w x y z 0 1 2 3 4 5 6 7 8 9 . , - space",
-					"save.DemoInstancesGenerator.features": "ss01"
+					"save.DemoInstancesGenerator.glyphs": "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z a b c d e f g h i j k l m n o p q r s t u v w x y z 0 1 2 3 4 5 6 7 8 9 . , - space",
 				}
 			)
 			self.w.name.set( Glyphs.defaults["save.DemoInstancesGenerator.name"] )
-			self.w.chars.set( Glyphs.defaults["save.DemoInstancesGenerator.chars"] )
-			self.w.features.set( Glyphs.defaults["save.DemoInstancesGenerator.features"] )
+			self.w.glyphs.set( Glyphs.defaults["save.DemoInstancesGenerator.glyphs"] )
 		except:
 			return False
 			
@@ -108,47 +95,54 @@ class DemoFontsGenerator ( object ):
 		
 	def ResetParameters ( self, sender):
 		del Glyphs.defaults["save.DemoInstancesGenerator.name"]
-		del Glyphs.defaults["save.DemoInstancesGenerator.chars"]
-		del Glyphs.defaults["save.DemoInstancesGenerator.features"]
+		del Glyphs.defaults["save.DemoInstancesGenerator.glyphs"]
 		self.w.name.set( Glyphs.defaults["save.DemoInstancesGenerator.name"] )
-		self.w.chars.set( Glyphs.defaults["save.DemoInstancesGenerator.chars"] )
-		self.w.features.set( Glyphs.defaults["save.DemoInstancesGenerator.features"] )
+		self.w.glyphs.set( Glyphs.defaults["save.DemoInstancesGenerator.glyphs"] )
 
 		
 		
 	def DemoFontsGeneratorParameters (self, sender):
-		demoName = self.w.name.get()
-		demoGlyphs = self.w.chars.get()
-		demoFeatures = self.w.features.get()
-		
-
-		# ----------- Customize -----------
-		"""
-		demoName = "Demo" # Demo or Trial or Test or whatever
-		demoGlyphs = ("%s%s .,- Aacute") %(string.ascii_letters, string.digits) # limited glyph set
-		demoFeatures = "" # limited features, if any
-		"""
-		# ---------------------------------
-
 
 		thisFont = Glyphs.fonts[0] # frontmost font
-		thisInstance = thisFont.instances
+
+
+		# glyphs and name to keep
+		demoName = self.w.name.get()
+		demoGlyphs = " %s " %(self.w.glyphs.get())
 
 
 		# list glyphs to remove
 		removeGlyphs = ""
-		for glyph in thisFont.glyphs:
-			if glyph.string not in demoGlyphs:
-				if glyph.name not in demoGlyphs:
-					removeGlyphs += ("%s, ") %(glyph.name)		
-					
-			
-		# list features to remove
+		removeClasses = ""
 		removeFeatures = ""
-		for feature in thisFont.features:
-			if feature.name not in demoFeatures:
-				removeFeatures += ("%s, ") %(feature.name)
-
+		
+		for glyph in thisFont.glyphs:
+			if (" %s ") %(glyph.string) not in demoGlyphs:
+				if (" %s ") %(glyph.name) not in demoGlyphs:
+					removeGlyphs += ("%s, ") %(glyph.name)	
+					
+					# list classes to remove
+					for GSClass in thisFont.classes:						
+						if ("%s ") %(glyph.name) in GSClass.code:
+							if ("%s, ") %(GSClass.name) not in removeClasses:
+								removeClasses += "%s, " %GSClass.name
+								
+								# if removed class is in a feature, list the feature to remove
+								for feature in thisFont.features:
+									if ("%s, ") %(feature.name) not in removeFeatures:
+										if (("%s ") %(GSClass.name) in feature.code) or (("%s' ") %(GSClass.name) in feature.code) or (((" %s;") %(GSClass.name) in feature.code)):
+											removeFeatures += ("%s, ") %(feature.name)	
+					
+					# list features to remove
+					for feature in thisFont.features:
+						if ("%s, ") %(feature.name) not in removeFeatures:
+							if (("%s ") %(glyph.name) in feature.code) or (("%s' ") %(glyph.name) in feature.code) or (((" %s;") %(glyph.name) in feature.code)):
+								removeFeatures += ("%s, ") %(feature.name)	
+												
+				
+		print("Removed classes: %s\n" %(removeClasses))
+		print(("Removed features: %s\n" %(removeFeatures)))
+						
 
 		# creates copies of active instances, adds limiting custom parameters, adds Demo naming
 		def copyInstances():
@@ -197,15 +191,15 @@ class DemoFontsGenerator ( object ):
 						newInstance.customParameters["fileName"] = "%s - %s" %(newInstance.customParameters["preferredFamilyName"], newInstance.customParameters["preferredSubfamilyName"])
 					
 						
-						#limit glyphs and features
+						#limit glyphs, features and classes
 						newInstance.customParameters["Remove Glyphs"] = removeGlyphs
 						newInstance.customParameters["Remove Features"] = removeFeatures
-						
+						newInstance.customParameters["Remove Classes"] = removeClasses
 						
 					# if demo already exists				
 					else:
 						if demoName in instance.name:
-							print("%s - already exists" %instance.name)
+							print("%s already exists" %instance.name)
 		copyInstances()
 
 DemoFontsGenerator()
