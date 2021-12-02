4f78221c474d251ff822234424c8b0cf9db2da83
Alex Slobzheninov <60325634+slobzheninov@users.noreply.github.com>
2021-12-02 16:45:54 -0500

    Add files via upload

---
+++ Testing/Highest & Lowest Glyphs.py
@@ -0,0 +1,25 @@
+#MenuTitle: Highest & Lowest Glyphs
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function, unicode_literals
+__doc__="""
+Finds tallest and lowest glyphs / Y coordinates.
+"""
+
+maxY = None
+minY = None
+highest = None
+lowest = None
+
+font = Glyphs.font
+for glyph in font.glyphs:
+	for layer in glyph.layers:
+		if not maxY or layer.bounds.origin.y + layer.bounds.size.height > maxY:
+			maxY = layer.bounds.origin.y + layer.bounds.size.height
+			highest = layer
+		if not minY or layer.bounds.origin.y < minY:
+			maxY = layer.bounds.origin.y
+			lowest = layer
+
+print( 'highest: %s' % maxY )
+print( 'lowest: %s' % minY )
+font.newTab( [highest, lowest] )
