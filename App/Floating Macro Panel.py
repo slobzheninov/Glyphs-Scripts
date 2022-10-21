71fb79daff3aa0ee0178169715af120885ebc27e
Alex Slobzheninov <60325634+slobzheninov@users.noreply.github.com>
2022-10-21 14:30:38 +0100

    Update Floating Macro Panel.py

--- App/Floating Macro Panel.py
+++ App/Floating Macro Panel.py
@@ -2,11 +2,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import division, print_function, unicode_literals
 __doc__="""
-Toggles Macro panel floating. Run when Macro window is open.
+Toggles Macro panel floating.
 """
 
 from AppKit import NSApp, NSFloatingWindowLevel, NSNormalWindowLevel
 
+Glyphs.showMacroWindow()
 for window in NSApp.windows():
 	if window.className() == 'GSMacroWindow':
 		# make normal
