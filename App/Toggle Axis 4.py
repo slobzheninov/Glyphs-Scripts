38431304f91e6def1e87ade6b91668bbd92874a7
Alex Slobzheninov <60325634+slobzheninov@users.noreply.github.com>
2023-05-25 02:51:56 +0100

    Faster algorithm

--- App/Toggle Axis 4.py
+++ App/Toggle Axis 4.py
@@ -1,56 +1,22 @@
 #MenuTitle: Toggle Axis 4
 # -*- coding: utf-8 -*-
 __doc__="""
-Toggles along masters across the 4th axis.
+Toggles along masters across the 1st axis.
 """
 from Foundation import NSUserDefaults, NSString
 
-
 AXIS = 3	# in Python we count from 0
+
+
+font = Glyphs.font
 
 
 # Add axes property to layers
 GSLayer.axes = property(lambda self: self.tempDataForKey_("axes"),
 	lambda self, value: self.setTempData_forKey_(value, "axes"))
 
-def getLayerAxes(layer):
-	layerAxes = []
-	if layer.isSpecialLayer and '{' in layer.name:
-		axesStr = layer.name.replace('{', '').replace('}', '').replace(",", " ").split()
-		for axis in axesStr:
-			layerAxes.append(float(axis))
-	else:
-		layerAxes = layer.master.axes
-	return layerAxes
 
-def getAxisValue(layer):
-	  return layer.axes[ AXIS ]
-
-def getRelatedLayers(selectedLayer):
-	selectedLayer.axes = getLayerAxes(selectedLayer)
-	relatedLayers = []
-
-	for layer in selectedLayer.parent.layers:
-		# skip the layer itself
-		if layer == selectedLayer:
-			continue
-		related = True
-		# get layer axes
-		layer.axes = getLayerAxes( layer )
-		for i, axis in enumerate( layer.axes ):
-			if i != AXIS:
-				if axis != selectedLayer.axes[ i ]:
-					related = False
-		# append related layers
-		if related is True:
-			relatedLayers.append( layer )
-	
-	# sort by the AXIS value
-	relatedLayers.sort( key = getAxisValue )
-	return relatedLayers
-
-
-def getNextLayer(selectedLayer, relatedLayers):
+def getNextMaster(selectedLayer, relatedLayers):
 	for layer in relatedLayers:
 		if layer.axes[ AXIS ] > selectedLayer.axes[ AXIS ]:
 			return layer
@@ -60,7 +26,7 @@ def getNextLayer(selectedLayer, relatedLayers):
 def getRelatedMasters(selectedMaster):
 	# find "related" layers along the axis
 	relatedMasters = []
-	for master in selectedMaster.font.masters:
+	for master in font.masters:
 		if master != selectedMaster:
 			related = True
 			for i, axis in enumerate( master.axes ):
@@ -74,7 +40,7 @@ def getRelatedMasters(selectedMaster):
 
 def toggleMaster(master):
 	relatedMasters = getRelatedMasters(master)
-	nextMaster = getNextLayer(master, relatedMasters)	
+	nextMaster = getNextMaster(master, relatedMasters)	
 
 	tab = master.font.currentTab
 	if tab:
@@ -82,33 +48,6 @@ def toggleMaster(master):
 	else:
 		master.font.masterIndex = master.font.masters.index( nextMaster )
 
-
-def getAllNextLayers(selectedLayers):
-	# get related layers for selected layers
-	allNextLayers = {}
-	for selectedLayer in selectedLayers:
-		selectedLayer.axes = getLayerAxes(selectedLayer)
-		ID = str(selectedLayer.axes)
-		if ID not in allNextLayers:
-			relatedLayers = getRelatedLayers(selectedLayer)
-			nextLayer = getNextLayer(selectedLayer, relatedLayers)
-			allNextLayers[ID] = nextLayer.axes
-	return allNextLayers
-
-
-def getLayerForAxes(axes, selectedLayer):
-	for layer in selectedLayer.parent.layers:
-		if layer.isSpecialLayer and hasattr(layer, 'axes'):
-			if layer.axes == axes:
-				return layer
-		else:
-			match = True
-			for i, axis in enumerate(axes):
-				if axis != layer.master.axes[i]:
-					match = False
-					continue
-			if match:
-				return layer
 
 def setMaster(font, tab, textCursor, textRange, toggle, newMasterIndex):
 	# select the layers
@@ -118,15 +57,14 @@ def setMaster(font, tab, textCursor, textRange, toggle, newMasterIndex):
 	font.masterIndex = newMasterIndex+toggle
 	font.masterIndex -= toggle
 
+
 def setMasterLayersToMaster(tab, newMaster, currentMaster=None):
-	# get user's selection
+	# get user's selection to reset later
 	currentTextCursor = tab.textCursor
 	currentTextRange = tab.textRange
-	font = newMaster.font
 	newMasterIndex = font.masters.index( newMaster )
 	if currentMaster == None:
 		currentMaster = newMaster
-
 	# toggle master to some other master and back, otherwise it doesn't apply
 	if newMaster == currentMaster:
 		if 0 < newMasterIndex:
@@ -158,52 +96,158 @@ def setMasterLayersToMaster(tab, newMaster, currentMaster=None):
 	tab.textRange = currentTextRange
 
 
-def toggleLayers(master, tab):
-	allNextLayers = getAllNextLayers(tab.layers)
-	# get layer indices
+def getAxisValue(layer):
+	return layer.axes[ AXIS ]
+
+
+def getLayerAxes(layer):
+	layerAxes = []
+	if '{' in layer.name:
+		axesStr = layer.name.replace('{', '').replace('}', '').replace(",", " ").split()
+		for axis in axesStr:
+			layerAxes.append(float(axis))
+	else:
+		layerAxes = layer.master.axes
+	layer.axes = layerAxes
+	return layerAxes
+
+
+def getRelatedLayers(specialLayer):
+	selectedLayerAxes = getLayerAxes(specialLayer)
+	
+	# ignore non {} layers
+	if not selectedLayerAxes:
+		return
+
+	# get related layers
+	relatedLayers = []
+	for layer in specialLayer.parent.layers:
+
+		# skip the layer itself
+		if layer == specialLayer:
+			continue
+
+		# get layer axes
+		layerAxes = getLayerAxes( layer )
+
+		# check if all axes match (except for the AXIS)
+		related = True
+		for i, axis in enumerate( layerAxes ):
+			if i != AXIS:
+				if axis != specialLayer.axes[ i ]:
+					related = False
+
+		# append related layers
+		if related is True:
+			relatedLayers.append( layer )
+	
+	# sort by the AXIS value
+	relatedLayers.sort( key = getAxisValue )
+	return relatedLayers
+
+
+def getNextSpecialLayerName(specialLayer):
+	relatedLayers = getRelatedLayers(specialLayer)
+
+	# {} layers
+	if relatedLayers:
+		nextSpecialLayer = getNextMaster(specialLayer, relatedLayers)
+		return nextSpecialLayer.name
+
+	# non {} layers
+	else:
+		return specialLayer.name
+
+
+def getNextMasterId(masterId):
+	master = font.masters[masterId]
+	relatedMasters = getRelatedMasters(master)
+	nextMaster = getNextMaster(master, relatedMasters)
+	return nextMaster.id
+
+
+def toggleMasterInTab(master, tab):
+	# get next master for current master and save it	
+	# go through each layer and if it’s different from master, find its next layer id, and save it too and swap.
+	# in the line above also check in the list instead of finding again.
+
+	cachedNextLayers = {}
+	tempTabLayers = copy(tab.layers)
+
+	# get next master to the tab master
+	nextMasterId = getNextMasterId(master.id)
+	nextMaster = font.masters[nextMasterId]
+
+	# cache next master and toggle tab master
+	cachedNextLayers[ master.id ] = nextMasterId
+	master.font.masterIndex = master.font.masters.index( nextMaster )
+
+	# get text selection
 	selectionStart = tab.textCursor
 	selectionEnd = tab.textCursor + tab.textRange
 
-	tabLayers = []
-	for i, layer in enumerate(tab.layers):
-		# no layers are selected or layer is within the selection
-		if tab.textRange == 0    or    selectionStart <= i < selectionEnd or (selectionStart == selectionEnd and i == selectionStart):
-			nextLayerAxes = allNextLayers[str(layer.axes)]
-			nextLayer = getLayerForAxes(nextLayerAxes, layer)
-			if nextLayer:
-				tabLayers.append(nextLayer)
+	# find layers different from tab master (check in the original/temp list)
+	for i, layer in enumerate(tempTabLayers):
+
+		# layer not selected (if any selection)
+		if not (tab.textRange == 0   or   selectionStart <= i < selectionEnd or (selectionStart == selectionEnd and i == selectionStart)):
+			continue
+
+		# other master layer
+		elif layer.isMasterLayer and layer.layerId != master.id:
+			# get next layer (either from cache or do cache it)
+			nextLayerId = cachedNextLayers.get( layer.layerId )
+			if nextLayerId is None:
+				nextLayerId = getNextMasterId( layer.layerId )
+				cachedNextLayers[ layer.layerId ] = nextLayerId
+			nextLayer = layer.parent.layers[ nextLayerId ]
+			tempTabLayers[i] = nextLayer
+
+		# special layer
+		elif layer.isSpecialLayer:
+			nextLayerName = cachedNextLayers.get( layer.name )
+			if nextLayerName is None:
+				nextLayerName = getNextSpecialLayerName(layer)
+				cachedNextLayers[ layer.name ] = nextLayerName
+			nextLayer = layer.parent.layers[ nextLayerName ]
+			tempTabLayers[i] = nextLayer
+
+		# control layer, skip
+		elif type(selectedLayer) == GSControlLayer:
+			continue
+
+		# current master layer, just append
 		else:
-			tabLayers.append(layer)
-	tab.layers = tabLayers
+			tempTabLayers[i] = layer.parent.layers[ nextMasterId ]
 
-	# reset master layers to current font master
-	relatedMasters = getRelatedMasters(master)
-	nextMaster = getNextLayer(master, relatedMasters)
-	setMasterLayersToMaster(tab, nextMaster)
+	# apply new layers only if any differ from tab master OR if any selection
+	if len(cachedNextLayers) > 1  or  0 < tab.textRange < len(tab.layers):
+		tab.layers = tempTabLayers
+		setMasterLayersToMaster(tab, nextMaster)
 
-	# reset master layers to current font master
-	setMasterLayersToMaster(tab, master)
+
 
 def getViewPortPosition(viewPort):
 	viewPortX = viewPort.origin.x
 	viewPortY = viewPort.origin.y
 	return viewPortX, viewPortY
 
+
 def setViewPortPosition(tab, viewPort, x, y):
 	viewPort.origin.x = x
 	viewPort.origin.y = y
 	tab.viewPort = viewPort
 
+
 def toggleAxis():
-	font = Glyphs.font
 	tab = font.currentTab
 	selectedMaster = font.selectedFontMaster
 
 	if AXIS is None or AXIS >= len(font.axes):
-		print("Axis not found")
+		print("Axis %s not found" % AXIS)
 		return
 
-	if not tab or not font.selectedLayers:
+	if not tab:
 		toggleMaster( selectedMaster )
 	
 	else:
@@ -212,9 +256,10 @@ def toggleAxis():
 		viewPortX, viewPortY = getViewPortPosition(viewPort)
 
 		# toggle layers and masters
-		toggleLayers( selectedMaster, tab )
+		toggleMasterInTab( selectedMaster, tab )
 
 		# restore viewport position
 		setViewPortPosition( tab, viewPort, viewPortX, viewPortY )
-		
+
+
 toggleAxis()
