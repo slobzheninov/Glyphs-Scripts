1755eefc9679b13fab32473ff937ee236572a2d8
Alex Slobzheninov <60325634+slobzheninov@users.noreply.github.com>
2025-12-28 03:14:13 +0100

    Fixed buggy behavior

--- App/Fit Zoom.py
+++ App/Fit Zoom.py
@@ -1,88 +1,39 @@
 #MenuTitle: Fit Zoom
 # -*- coding: utf-8 -*-
 from __future__ import division, print_function, unicode_literals
-__doc__ = """
-Fit text in the current tab to full screen. Works weirdly in Glyphs 3.
+__doc__="""
+Fit the selected layer in the current tab on the screen. With Text tool on, fit the whole text by width.
 """
 
-from GlyphsApp import Glyphs
+if Font and Font.currentTab and Layer:
+	Glyphs.clearLog()
+	font = Font
+	textTool = Glyphs.currentDocument.windowController().toolDrawDelegate().className() in ['GlyphsToolText', 'GlyphsToolHand']
+	
+	# Fit paragraph by width
+	if textTool:
 
-Glyphs.clearLog()
-font = Glyphs.font
-tab = font.currentTab
-viewPort = tab.viewPort
-
-# check if text tool is selected
-if Glyphs.currentDocument.windowController().toolDrawDelegate().className() in ['GlyphsToolText', 'GlyphsToolHand']:
-	textTool = True
-else:
-	textTool = False
-
-
-# text/hand tool > fit all
-if textTool:
-	# set scale to 1
-	tab.scale = 1
-	# set margin (relative)
-	M = tab.bounds.size.width / 30
-	# get screen ratio
-	screenRatio = viewPort.size.width / viewPort.size.height
-	# get text ratio
-	textRatio = tab.bounds.size.width / tab.bounds.size.height
-
-	# fit by width
-	if textRatio > screenRatio:
-		# width
-		viewPort.size.width = tab.bounds.size.width + M * 2
-		# x
-		viewPort.origin.x = -M
-		# y (centered)
-		viewPort.origin.y = -viewPort.size.height / 2 - tab.bounds.size.height / 2
-
-	# fit by height
-	else:
-		# height
-		viewPort.size.height = tab.bounds.size.height + M * 2
-		# y
-		viewPort.origin.y = -tab.bounds.size.height
-		# x (centered)
-		viewPort.origin.x = -viewPort.size.width / 2 + tab.bounds.size.width / 2
-
-	# apply changes
-	tab.viewPort = viewPort
-
-
-# other tools > fit current layer
-else:
-	if font.selectedLayers:
-		# set scale to 1
-		tab.scale = 1
-
-		layer = font.selectedLayers[0]
-		# get layer's origin position in tab
+		# Adjust scale to fit paragraph by width with some margin
+		tab = font.currentTab
+		tab.scale *= (viewPort.size.width / tab.bounds.size.width * .95)
+		font.parent.windowController().activeEditViewController().graphicView().updateBoundsFromFrameView()
+		viewPort = tab.viewPort
 		activePos = font.parent.windowController().activeEditViewController().graphicView().activePosition()
-		# set margin (absolute)
-		M = 50
-		# get screen ratio
-		screenRatio = viewPort.size.width / viewPort.size.height
-		# get current layer ratio (descender-ascender)
-		layerHeight = abs(layer.master.descender) + layer.master.ascender
-		textRatio = layer.width / layerHeight
 
-		# # fit by width
-		if textRatio > screenRatio:
-			# width
-			viewPort.size.width = layer.width + M * 2
+		# Margin
+		M = (viewPort.size.width - tab.bounds.size.width) / 2
 
-		# fit by height
-		else:
-			# height
-			viewPort.size.height = layerHeight + M * 2
+		# Set position
+		minY = -viewPort.size.height + M
+		maxY = -tab.bounds.size.height - M
+		centerY = -viewPort.size.height/2 + activePos.y
+		viewPort.origin.y = min(max(centerY, maxY), minY)
+		viewPort.origin.x = -M
 
-		# x (centered)
-		viewPort.origin.x = activePos.x - viewPort.size.width / 2 + layer.width / 2
-		# y (centered)
-		viewPort.origin.y = -viewPort.size.height / 2 + activePos.y + (layer.master.ascender + layer.master.descender) / 2
-
-		# apply changes
+		# Apply position
 		tab.viewPort = viewPort
+
+	# Fit the selected layer on screen
+	else:	
+		font.parent.windowController().activeEditViewController().graphicView().zoomFitInWindow_(None)
+		font.parent.windowController().activeEditViewController().graphicView().zoomOut_(None) # Add some margin
